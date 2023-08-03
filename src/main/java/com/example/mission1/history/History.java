266c4f2be6e71d73a63aed05efacf4b308d98676
HanSeul <w0w1278@naver.com>
2023-08-03 19:10:36 +0900

    8/3 history table 틀 만들기

--- src/main/java/com/example/mission1/history/History.java
+++ src/main/java/com/example/mission1/history/History.java
@@ -1,4 +1,15 @@
 package com.example.mission1.history;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Getter
+@Setter
 public class History {
+    private int id;
+    private double lat;
+    private double lnt;
+    private Date inquiryDate;
 }
