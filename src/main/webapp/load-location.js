721f4a630e91acae436f39dbd5f999a589052b03
HanSeul <w0w1278@naver.com>
2023-08-04 15:53:36 +0900

    8/4 History db에 사용자 위치(위경도) 저장기능 완료. 코드는 조금 더 수정이 필요

--- src/main/webapp/load-location.js
+++ src/main/webapp/load-location.js
@@ -1,49 +1,71 @@
-
+class UserLocation {
+    static lat = -1.0;
+    static lnt = -1.0;
+}
 function loadGeo() {
     navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
 }
 function onGeoOk(position){
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    document.getElementById("LAT").value = lat;
-    document.getElementById("LNT").value = lng;
-    console.log(lat);
-    console.log(lng);
+    UserLocation.lat = position.coords.latitude;
+    UserLocation.lnt = position.coords.longitude;
+    document.getElementById("LAT").value = UserLocation.lat;
+    document.getElementById("LNT").value = UserLocation.lnt;
+
+    if (UserLocation.lat != -1.0 && UserLocation.lnt != -1.0) {
+        sendDataToServer();
+    }
 }
 function onGeoError(){
     alert("Can't find you. No weather for you.");
 }
 
-function calDistance() {
-    const lat = document.getElementById("LAT").value;
-    const lng = document.getElementById("LNT").value;
-
-    // 서버에 위치 정보를 전달하는 AJAX 요청
-    $.ajax({
-        type: "POST",
-        url: "/getNearbyWifi",
-        data: {
-            lat: lat,
-            lng: lng,
-        },
-        success: function (response) {
-            // 서버로부터 받은 결과를 처리하는 로직
-            // 예를 들어, 테이블에 결과 데이터를 동적으로 추가하는 등의 동작을 수행
-
-            // 예시로 가까운 WiFi 정보를 테이블에 추가하는 코드
-            const table = document.getElementById("wifiInfos");
-            table.innerHTML = ""; // 테이블 초기화
-
-            response.forEach(function (wifi) {
-                const row = table.insertRow();
-                row.insertCell().innerText = wifi.distance;
-                row.insertCell().innerText = wifi.manageNo;
-                row.insertCell().innerText = wifi.borough;
-                // ... 나머지 WiFi 정보를 셀에 추가
-            });
-        },
-        error: function (error) {
-            alert("Failed to get nearby WiFi data.");
+// AJAX 요청 보내기
+function sendDataToServer() {
+    const xhr = new XMLHttpRequest();
+    const url = "/History";
+    const currentTime = new Date().toISOString();
+    const data = {
+        lat: UserLocation.lat.toString(),
+        lnt: UserLocation.lnt.toString(),
+        inquiryDate: currentTime
+    };
+    console.log(UserLocation.lat);
+    console.log(UserLocation.lnt);
+    console.log(data);
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                console.log("데이터가 성공적으로 추가되었습니다.");
+            } else {
+                console.error("데이터 추가에 실패하였습니다.");
+            }
         }
-    });
+    };
+
+    xhr.send(JSON.stringify(data)); // 데이터를 서버로 전송
+}
+function getNearbyWifiInfo() {
+    if (document.getElementById("LAT").value == 0.0 && document.getElementById("LNT").value == 0.0 ) {
+        alert("위치 정보를 가져오거나 입력해주세요.");
+    }
+
+    if (!isNaN(lat) && !isNaN(lnt)) {
+        // AJAX로 Java Servlet에 위치 정보 전송
+        fetch(`/getWifiInfo?lat=${lat}&lnt=${lnt}`)
+            .then(response => {
+                if (response.ok) {
+                    alert("가까운 Wi-Fi 정보 업데이트 완료!");
+                } else {
+                    alert("가까운 Wi-Fi 정보 업데이트 실패!");
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert("가까운 Wi-Fi 정보 업데이트 실패!");
+            });
+    } else {
+        alert("위치 정보를 가져오거나 입력해주세요.");
+    }
 }
