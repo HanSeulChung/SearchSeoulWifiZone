705c7c37b6e2e2470cf6e7248bcf71a756849877
HanSeul <w0w1278@naver.com>
2023-08-07 07:51:52 +0900

    8/7 삭제 버튼 누르면 history db와 화면에 반영, 그러나 doPost와 doDelete 중 무엇이 더 올바르지 모르겠음-> 시간이 남으면 다시 시도

--- src/main/java/com/example/mission1/history/HistoryDeleteServlet.java
+++ src/main/java/com/example/mission1/history/HistoryDeleteServlet.java
@@ -7,38 +7,35 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 @WebServlet("/history-delete")
 public class HistoryDeleteServlet extends HttpServlet {
+    private final HistoryRepository historyRepository = new HistoryRepository();
+//    @Override
+//    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        // history.jsp에서 넘어온 ID 파라미터를 받음
+//        System.out.println(request);
+//        String idStr = request.getParameter("id");
+//        if (idStr == null || idStr.isEmpty()) {
+//            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "ID 값이 필요합니다.");
+//            return;
+//        }
+//
+//        try {
+//            int id = Integer.parseInt(idStr);
+//            historyRepository.deleteHistoryById(id);
+//            System.out.printf("ID=%d 가 삭제되었습니다", id);
+//            response.setStatus(HttpServletResponse.SC_OK);
+//        } catch (NumberFormatException e) {
+//            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "잘못된 ID 값입니다.");
+//        }
+//    }
+
     @Override
-    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         // history.jsp에서 넘어온 ID 파라미터를 받음
-        String bodyJson = "";
-
-        StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader br = null;
-        //한줄씩 담을 변수
-        String line = "";
-
-        try {
-            //body내용 inputstream에 담는다.
-            InputStream inputStream = request.getInputStream();
-            if (inputStream != null) {
-                br = new BufferedReader(new InputStreamReader(inputStream));
-                //더 읽을 라인이 없을때까지 계속
-                while ((line = br.readLine()) != null) {
-                    System.out.println(line);
-                }
-            }else {
-                System.out.println("Data 없음");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println(request);
         String idStr = request.getParameter("id");
         if (idStr == null || idStr.isEmpty()) {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, "ID 값이 필요합니다.");
@@ -49,7 +46,6 @@ public class HistoryDeleteServlet extends HttpServlet {
             int id = Integer.parseInt(idStr);
             HistoryRepository historyRepository = new HistoryRepository();
             historyRepository.deleteHistoryById(id);
-            System.out.printf("ID=%d 가 삭제되었습니다", id);
             response.setStatus(HttpServletResponse.SC_OK);
         } catch (NumberFormatException e) {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, "잘못된 ID 값입니다.");
--- src/main/webapp/history/history.jsp
+++ src/main/webapp/history/history.jsp
@@ -38,32 +38,6 @@
             color: white;
         }
     </style>
-
-    <script>
-        function confirmDelete(id) {
-            if (confirm("삭제하시겠습니까?")) {
-                // 확인 버튼을 누른 경우
-                // 서블릿으로 삭제 요청을 보냄
-                const xhr = new XMLHttpRequest();
-                const url = "history-delete?id=" + id;
-                xhr.open("DELETE", url, true);
-                xhr.onreadystatechange = function () {
-                    if (xhr.readyState === XMLHttpRequest.DONE) {
-                        if (xhr.status === 200) {
-                            // 성공적으로 삭제되면 history.jsp로 리다이렉트
-                            window.location.reload();
-                        } else {
-                            // 삭제에 실패한 경우 예외 처리
-                            alert("삭제에 실패하였습니다.");
-                        }
-                    }
-                };
-                xhr.send();
-            } else {
-                // 취소 버튼을 누른 경우 아무 동작 안 함
-            }
-        }
-    </script>
 </head>
 <body>
 <h1><%= "위치 히스토리 목록" %></h1>
@@ -101,9 +75,10 @@
         <td><%= history.getLat() %></td>
         <td><%= history.getLnt() %></td>
         <td><%= history.getInquiryDate() %></td>
-        <td><button onclick="confirmDelete(<%= history.getId() %>)">삭제</button></td>
+        <td><button onclick="js:confirmDelete(<%= history.getId() %>)">삭제</button></td>
     </tr>
     <% } } %>
 </table>
+<script src = "../delete-history.js" charset="UTF-8"></script>
 </body>
 </html>
